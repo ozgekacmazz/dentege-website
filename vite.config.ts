6649ecbdde092ec066ae0e70ff53b7a075ab315a
ozge.asya <ozkacmz8@gmail.com>
2026-02-10 22:28:07 +0300

    Fix Vite config: enable react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
 export default defineConfig({
+  plugins: [react()],
   server: {
     allowedHosts: [
-      'transilient-forfeitable-haywood.ngrok-free.dev'
+      'transient-forfeitable-haywood.ngrok-free.dev'
     ]
   }
 })
